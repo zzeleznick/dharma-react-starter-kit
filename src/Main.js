d90df9853505186447d4da483291cd26b00a65b5
zzeleznick <zzeleznick@gmail.com>
2018-02-18 01:32:08 -0700

    fix router issue

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Switch, Route } from 'react-router-dom'
 import App from './App'
 import Header from './Header'
 import OrderBook from './components/OrderBook'
+
+import { Provider as AlertProvider } from 'react-alert'
+import AlertTemplateCustom from './AlertTemplateCustom'
 
 // The Main component renders one of the three provided
 // Routes (provided that one matches). Both the /roster
@@ -10,11 +13,28 @@ import OrderBook from './components/OrderBook'
 // with /roster or /schedule. The / route will only match
 // when the pathname is exactly the string "/"
 
+// optional cofiguration
+const options = {
+  position: 'bottom center',
+  timeout: 0,//5000,
+  offset: '30px',
+  transition: 'scale'
+}
+class WrappedApp extends Component  {
+    render() {
+        return (
+          <AlertProvider template={AlertTemplateCustom} {...options}>
+           <App />
+         </AlertProvider>
+    )
+  }
+}
+
 const Main = () => (
   <main>
   <Header />
     <Switch>
-      <Route exact path='/' component={App}/>
+      <Route exact path='/' component={WrappedApp}/>
       <Route path='/orders' component={OrderBook}/>
     </Switch>
   </main>
--- src/Root.js
+++ src/Root.js
@@ -1,26 +1,15 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
-import { Provider as AlertProvider } from 'react-alert'
-// import Main from './Main'
+import Main from './Main'
 import App from './App'
 import ErrorBoundary from './ErrorBoundary'
-import AlertTemplateCustom from './AlertTemplateCustom'
 
-// optional cofiguration
-const options = {
-  position: 'bottom center',
-  timeout: 0,//5000,
-  offset: '30px',
-  transition: 'scale'
-}
 
 class Root extends Component  {
   render () {
     return (
       <ErrorBoundary>
-        <AlertProvider template={AlertTemplateCustom} {...options}>
-          <App />
-        </AlertProvider>
+        <Main />
       </ErrorBoundary>
     )
   }
